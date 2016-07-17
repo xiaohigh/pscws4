8b286e72ff616f4707430e9f1edecebe0fcdb9d8
vagrant <vagrant@homestead>
2016-07-17 12:13:09 +0000

    init version

---
+++ src/Pscws4.php
@@ -0,0 +1,32 @@
+<?php 
+		
+	/**
+	 * @author xiaohigh
+	 * @email  xiaohigh@163.com
+	 * @time   2016-07-16
+	 */
+	namespace xiaohigh\Pscws4;
+
+	class Pscws
+	{
+
+		/**
+		 * 构造方法
+		 */
+		public function __construct()
+		{
+
+		}
+
+		/**
+		 * 功能实现
+		 */
+		public function run()
+		{
+			echo 'Programming Runing';
+		}
+
+
+	}
+
+
